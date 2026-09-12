5a453f87632eab5ce7298289a1a24f078fec7b52
Hadeel Elmutasim <100997851+hadeel303elmutasim@users.noreply.github.com>
2026-09-13 02:11:18 +0400

    Remove updates 3

--- app.js
+++ app.js
@@ -7,22 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const publicDirectory =  path.join(__dirname , './public')
-    app.use (express.static (publicDirectory))
-
-// Set up Handlebars View Engine
+app.use (express.static (publicDirectory))
 app.set('view engine', 'hbs');
  const viewsDirectory = path.join (__dirname , './views')
  app.set('views', viewsDirectory);
 
-// Route: Transferring data from app.js to index.hbs
 app.get('/', (req, res) => {
-    // Simulated data (like electronic products)
      res.render('index' , { 
         title: "Forecast Page"
     });
-
 });
-// ///////////////////////////////////////////////////////////////
 const geocode = require('./data/geocode')
 const forecast = require('./data/forecast')
 app.get('/weather',(req,res)=>{
@@ -33,7 +27,6 @@ app.get('/weather',(req,res)=>{
     }
     geocode(req.query.address,(error,data)=>{
         if(error){
-            // shorthand property error:error
             return res.send({error})
         }
         forecast(data.latitude,data.longtitude,(error,forecastData)=>{
